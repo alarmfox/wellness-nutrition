9970e1bec82984242ce056bb98e480328a4d4e5f
Giuseppe <capassog97@gmail.com>
2023-02-06 17:09:31 +0100

    Closed userPage

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,48 +1,57 @@
 import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
-import { CreateUserSchema } from "../../../utils/user.schema";
-import { adminProtectedProcedure, createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { z } from "zod";
+import { CreateUserSchema, UpdateUserSchema } from "../../../utils/user.schema";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
-    getCurrent: protectedProcedure.query(({ ctx }) => {
-        return ctx.prisma.user.findFirst({
-            where: {
-                id: ctx.session.user.id
-            }
+  getCurrent: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findFirst({
+      where: {
+        id: ctx.session.user.id
+      }
+    })
+  }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findMany();
+  }),
+
+  create: publicProcedure.
+    input(CreateUserSchema).
+    mutation(async ({ ctx, input }) => {
+      try {
+        return await ctx.prisma.user.create({
+          data: input,
         })
+      } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+          // The .code property can be accessed in a type-safe manner
+          if (e.code === 'P2002') {
+            throw new TRPCError({
+              code: 'CONFLICT'
+            })
+          }
+        }
+        throw e;
+      }
     }),
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.user.findMany();
-    }),
-
-    create: publicProcedure.
-        input(CreateUserSchema).
-        mutation(async ({ ctx, input }) => {
-            try {
-                return await ctx.prisma.user.create({
-                    data: {
-                        firstName: input.firstName,
-                        lastName: input.lastName,
-                        address: input.address,
-                        cellphone: input.cellphone,
-                        expiresAt: input.expiresAt,
-                        remainingAccesses: input.remainingAccesses,
-                        email: input.email
-                    },
-                })
-            } catch (e) {
-                if (e instanceof Prisma.PrismaClientKnownRequestError) {
-                    // The .code property can be accessed in a type-safe manner
-                    if (e.code === 'P2002') {
-                        throw new TRPCError({
-                            code: 'CONFLICT'
-                        })
-                    }
-                }
-                throw e;
-            }
+  delete: publicProcedure.input(z.array(z.string())).mutation(({ ctx, input}) => {
+    return ctx.prisma.user.deleteMany({
+      where: {
+        id: {
+          in: input
+        },
+      }
+    })
+  }),
+  update: publicProcedure.input(UpdateUserSchema).mutation(({ ctx, input } ) => {
+    return ctx.prisma.user.update({
+      where: {
+        id: input.id
+      },
+      data: input
+    })
+  })
 
 
-        }),
-});
-
+})
--- src/utils/user.schema.ts
+++ src/utils/user.schema.ts
@@ -5,11 +5,25 @@ export const CreateUserSchema = z.object({
     firstName: z.string(),
     lastName: z.string(),
     email: z.string(),
-    cellphone: z.string(),
+    cellphone: z.string().nullable(),
     remainingAccesses: z.number(),
     expiresAt: z.date(),
     subType: z.nativeEnum(SubType), 
-    address: z.string()
+    address: z.string(),
+    medOk: z.boolean()
 })
 
+
+export const UpdateUserSchema = z.object({
+    id: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+    cellphone: z.string().nullable(),
+    remainingAccesses: z.number(),
+    expiresAt: z.date(),
+    subType: z.nativeEnum(SubType), 
+    address: z.string(),
+    medOk: z.boolean()
+})
 export type CreateUserModel = z.infer<typeof CreateUserSchema>;
+export type UpdateUserModel = z.infer<typeof UpdateUserSchema>;
