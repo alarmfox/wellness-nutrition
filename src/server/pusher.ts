fa7be3722fffcfd7b9770e50d9e732f7d52de6a5
Giuseppe <capassog97@gmail.com>
2023-02-12 12:52:15 +0100

    Fix: pusher from env

--- src/server/pusher.ts
+++ src/server/pusher.ts
@@ -1,11 +1,19 @@
 import Pusher from "pusher";
 import { env } from "../env/server.mjs";
 
-
 export const pusher = new Pusher({
-  appId: env.PUSHER_APP_ID, 
+  appId: env.PUSHER_APP_ID,
   key: env.PUSHER_APP_KEY,
   secret: env.PUSHER_APP_SECRET,
   cluster: env.PUSHER_APP_CLUSTER,
   useTLS: env.PUSHER_APP_USE_TLS === 'true',
-})
+});
+
+/* const pusher = new Pusher({
+  appId: "1552406",
+  key: "a017f5abd9769da5b770",
+  secret: "17d3e9411e7a65ce6e15",
+  cluster: "eu",
+  useTLS: true
+});
+ */
